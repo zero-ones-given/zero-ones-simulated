b1ade756c3d85437b247a25188f0da087969dafb
Tuure Kaunisto <tuure.kaunisto@gmail.com>
2020-06-02 01:49:41 +0300

    Stream captured images via mjpeg

--- Assets/Scripts/StreamCameraController.cs
+++ Assets/Scripts/StreamCameraController.cs
@@ -1,51 +1,51 @@
 ﻿using UnityEngine;
+using System;
 using System.IO;
 using System.Threading;
+using System.Diagnostics;
 
 public class StreamCameraController : MonoBehaviour
 {
-    public int captureWidth = 1080;
-    public int captureHeight = 1080;
+    private int _captureWidth = 1080;
+    private int _captureHeight = 1080;
 
-    Camera streamCamera;
-    Rect rect;
-    private int imageNumber = 0;
+    private Camera _streamCamera;
+    private VideoServer _videoServer;
+    private Stopwatch _stopWatch;
 
     void Start()
     {
-        rect = new Rect(0, 0, captureWidth, captureHeight);
-        streamCamera = this.GetComponent<Camera>();
-        streamCamera.enabled = false;
+        _streamCamera = this.GetComponent<Camera>();
+        _streamCamera.enabled = false;
+        _videoServer = new VideoServer();
+        _videoServer.Start();
+        _stopWatch = new Stopwatch();
+        _stopWatch.Start();
     }
 
     void Update()
     {
-        if (!Input.GetKeyDown("space")) {
-            return;
+        if (_stopWatch.Elapsed.Milliseconds > 66) {
+            RenderFrame();
+            _stopWatch.Reset();
+            _stopWatch.Start();
         }
-        RenderTexture renderTexture = new RenderTexture(captureWidth, captureHeight, 24);
-        Texture2D screenShot = new Texture2D(captureWidth, captureHeight, TextureFormat.RGB24, false);
-        streamCamera.targetTexture = renderTexture;
-        streamCamera.Render();
-        RenderTexture.active = renderTexture;
-        screenShot.ReadPixels(rect, 0, 0);
+    }
 
-        streamCamera.targetTexture = null;
+    void RenderFrame()
+    {
+        RenderTexture renderTexture = new RenderTexture(_captureWidth, _captureHeight, 24);
+        Texture2D screenShot = new Texture2D(_captureWidth, _captureHeight, TextureFormat.RGB24, false);
+        _streamCamera.targetTexture = renderTexture;
+        _streamCamera.Render();
+        RenderTexture.active = renderTexture;
+        screenShot.ReadPixels(new Rect(0, 0, _captureWidth, _captureHeight), 0, 0);
+
+        _streamCamera.targetTexture = null;
         RenderTexture.active = null;
         Destroy(renderTexture);
 
-        byte[] fileData = screenShot.EncodeToJPG();
-
-        string fileName = $"{Application.dataPath}/../{imageNumber}.jpg";
-        new System.Threading.Thread(() =>
-        {
-            // create file and write optional header with image bytes
-            var f = File.Create(fileName);
-            f.Write(fileData, 0, fileData.Length);
-            f.Close();
-            Debug.Log($"Saved screenshot {fileName} (size {fileData.Length})");
-        }).Start();
-
-        imageNumber++;
+        var fileData = screenShot.EncodeToJPG();
+        _videoServer.LatestFrame = fileData;
     }
 }
