db66f818637f0d5f9377c77bbfe23144363a62e9
Tuure Kaunisto <tuure.kaunisto@gmail.com>
2020-06-02 01:49:41 +0300

    Stream captured images via mjpeg

--- Assets/Scripts/VideoServer.cs
+++ Assets/Scripts/VideoServer.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System;
 using System.Text;
 using System.Net.Sockets;
 using System.Net;
@@ -6,52 +7,66 @@ using System.Threading;
  
 class VideoServer
 {
+    public byte[] LatestFrame = {};
+
     public void Start()
     {
-        const string headers = "HTTP/1.1 200 OK\r\n" +
-            "Content-Type: multipart/x-mixed-replace; boundary=--boundary\r\n";
         const int port = 8080;
-        Debug.Log("Starting http server: " + port);
+        Debug.Log($"Starting http server: {port}");
+        var applicationPath = Application.dataPath;
 
-        TcpListener tcpListener = new TcpListener(IPAddress.Any, port);
+        var tcpListener = new TcpListener(IPAddress.Any, port);
         tcpListener.Start();
 
-        byte[] image0 = System.IO.File.ReadAllBytes($"{Application.dataPath}/../0.jpg");
-        byte[] image1 = System.IO.File.ReadAllBytes($"{Application.dataPath}/../1.jpg");
-
-        new System.Threading.Thread(() =>
+        new Thread(() =>
         {
-            byte[] EncodeString(string str) {
-                return Encoding.ASCII.GetBytes(str);
-            }
-            string GetImageHeaders(byte[] image) {
-                return "\r\n--boundary\r\nContent-Type: image/jpeg\r\n" +
-                    $"Content-Length: {image.Length.ToString()}\r\n\r\n";
-            }
-            void WriteString(string str, NetworkStream stream) {
-                byte[] encodedString = EncodeString(str);
-                stream.Write(encodedString, 0, encodedString.Length);
-            }
-
             while (true)
             {
-                TcpClient client = tcpListener.AcceptTcpClient();
-                NetworkStream stream = client.GetStream();
-
-                WriteString(headers, stream);
-
-                int frame = 0;
-                while(true) {
-                    byte[] image = frame % 2 == 0 ? image0 : image1;
-                    WriteString(GetImageHeaders(image), stream);
-                    stream.Write(image, 0, image.Length);
-                    WriteString("\r\n", stream);
-                    Thread.Sleep(1000);
-                    frame++;
-                }
-
-                client.Close();
+                var client = tcpListener.AcceptTcpClient();
+                var videoThread = new Thread(() => StreamVideo(client, applicationPath));
+                videoThread.Start();
             }
         }).Start();
+    }
+
+    private string GetImageHeaders(byte[] image)
+    {
+        return "\r\n--boundary\r\nContent-Type: image/jpeg\r\n" +
+            $"Content-Length: {image.Length}\r\n\r\n";
+    }
+
+    private void WriteString(string str, NetworkStream stream)
+    {
+        var encodedString = Encoding.ASCII.GetBytes(str);
+        stream.Write(encodedString, 0, encodedString.Length);
+    }
+
+    private void StreamVideo(TcpClient client, string applicationPath)
+    {
+        using(client)
+        using(var stream = client.GetStream())
+        {
+            const string headers = "HTTP/1.1 200 OK\r\n" +
+            "Content-Type: multipart/x-mixed-replace; boundary=--boundary\r\n";
+
+            WriteString(headers, stream);
+
+            while(true)
+            {
+                try
+                {
+                    WriteString(GetImageHeaders(LatestFrame), stream);
+                    stream.Write(LatestFrame, 0, LatestFrame.Length);
+                    WriteString("\r\n", stream);
+                    Thread.Sleep(66);
+                }
+                catch (Exception ex)
+                {
+                    // Disconnected?
+                    Debug.Log($"Encountered error: {ex.Message}");
+                    break;
+                }
+            }
+        };
     }
 }
