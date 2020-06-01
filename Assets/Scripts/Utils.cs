c4528e3139464bf9e6a411ee074b06f41572ca76
Tuure Kaunisto <tuure.kaunisto@gmail.com>
2020-06-02 02:08:22 +0300

    Add MapAndLimit Util method

--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -1,6 +1,17 @@
+using System;
+
 public static class Utils {
     public static float Map(int value, int fromLow, int fromHigh, int toLow, int toHigh)
     {
         return (float)(value - fromLow) * (toHigh - toLow) / (fromHigh - fromLow) + toLow;
+    }
+
+    public static float Limit(float value, float min, float max) {
+        return Math.Max(Math.Min(value, max), min);
+    }
+
+    public static float MapAndLimit(int value, int fromLow, int fromHigh, int toLow, int toHigh)
+    {
+        return Limit(Map(value, fromLow, fromHigh, toLow, toHigh), toLow, toHigh);
     }
 }
